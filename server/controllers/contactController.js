a2e0e8a1d6bc474788df518c432caa8bc6918b0a
AaiswaryaPM <aaiswaribm.learning@gmail.com>
2026-05-20 22:37:35 +0530

    Updated backend

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -22,15 +22,10 @@ export const sendMessage = async (req, res) => {
 
     // Nodemailer Transport
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
       },
     });
 
