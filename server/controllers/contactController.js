6dc9b01cbcae695057db437a28be40594f001a21
AaiswaryaPM <aaiswaribm.learning@gmail.com>
2026-05-21 22:01:53 +0530

    Updated the API

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -20,15 +20,9 @@ export const sendMessage = async (req, res) => {
 
     console.log("Saved to MongoDB");
 
-    await transporter.verify();
-    console.log("SMTP Connected");
-    
     // Nodemailer Transport
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
