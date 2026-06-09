a567230132dffa07e5b42cfefe309e69d456aff0
AaiswaryaPM <aaiswaribm.learning@gmail.com>
2026-06-09 20:53:42 +0530

    Added HTTP Methods

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -21,3 +21,78 @@ export const getProjects = async (req, res) => {
 
   }
 };
+
+export const createProject = async (req, res) => {
+  try {
+
+    const project = await Project.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: project,
+      message: "Project created successfully",
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server Error",
+    });
+
+  }
+};
+
+export const updateProject = async (req, res) => {
+  try {
+
+    const project =
+      await Project.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+        }
+      );
+
+    return res.status(200).json({
+      success: true,
+      data: project,
+      message: "Project updated successfully",
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server Error",
+    });
+
+  }
+};
+
+export const deleteProject = async (req, res) => {
+  try {
+
+    await Project.findByIdAndDelete(
+      req.params.id
+    );
+
+    return res.status(200).json({
+      success: true,
+      data: null,
+      message: "Project deleted successfully",
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server Error",
+    });
+
+  }
+};
--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,10 +1,21 @@
 import express from "express";
 
-import { getProjects }
+import {
+  getProjects,
+  createProject,
+  updateProject,
+  deleteProject,
+}
 from "../controllers/projectController.js";
 
 const router = express.Router();
 
 router.get("/", getProjects);
+
+router.post("/", createProject);
+
+router.put("/:id", updateProject);
+
+router.delete("/:id", deleteProject);
 
 export default router;
